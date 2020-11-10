58e92ab355fb6fd411731fefc02b806e7440d17c
Chico Finando Sutan Ibrahim 14 <finandochico@gmail.com>
2020-11-10 16:19:13 +0700

    -header done -home done

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Haina Company Profile">
+    <meta name="author" content="Chico Finando">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>Diamond</title>
+
+    
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+
+    <link rel="stylesheet" href="css/owl-carousel.css">
+    <link rel="stylesheet" href="css/custom.css">
+
+</head>
+<body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+    <!-- ***** Preloader End ***** -->
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky background-header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="container">
+                    <div class="row">
+                    <div class="col-6">
+                        <a href="" class="logo">
+                            <img class="logo-img" src="images/logo.png"></img>
+                        </a>
+                    </div>
+                    <div class="col-6">
+                        <div class="logo-right">
+                            <img class="logo-img" src="images/logo.png"></img>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="container-fluid nav-bg">
+                    <nav class="main-nav">
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li><a href="#welcome" class="menu-item">Home</a></li>
+                            <li><a href="#" class="menu-item">Company Profile</a></li>
+                            <li><a href="#" class="menu-item">Our Products</a></li>
+                            <li><a href="#" class="menu-item">Our Facilities</a></li>
+                            <li><a href="#" class="menu-item">Major Project</a></li>
+                            <li><a href="#" class="menu-item">Administration</a></li>
+                            <li><a href="#" class="menu-item">News</a></li>
+                            <li><a href="#" class="menu-item">Careers</a></li>
+                            <li><a href="#" class="menu-item">Contact Us</a></li>
+                        </ul>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+    
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="margin-top:160px">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img class="d-block w-100 img-slider" src="images/slider1.jpg" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                        <img class="d-block w-100 img-slider" src="images/slider2.jpg" alt="Second slide">
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="row" style="margin-top:50px;">
+            <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+                <div class="tab-menu">
+                    <img class="img-menu" src="images/slider1.jpg">
+                    <h4>Company Profile</h4>
+                    <p>find out more about us and our vision</p>
+                    
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+                <div class="tab-menu">
+                    <img class="img-menu" src="images/slider1.jpg">
+                    <h4>Our Product</h4>
+                    <p>we serve plastics supply and tarpaulin products</p>
+                    
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+                <div class="tab-menu">
+                    <img class="img-menu" src="images/slider1.jpg">
+                    <h4>Our Facilities</h4>
+                    <p>our facilities continues to develop to meet our customers requirements</p>
+                    
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+                <div class="tab-menu">
+                    <img class="img-menu" src="images/slider1.jpg">
+                    <h4>Major Project</h4>
+                    <p>highlight and our track record to assure you of our capabilities</p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <!-- jQuery -->
+    <script src="js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="js/owl-carousel.js"></script>
+    <script src="js/scrollreveal.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/imgfix.min.js"></script>
+
+    <!-- Global Init -->
+    <script src="js/custom.js"></script>
+
+    
+
+
+
+
+</body>
+</html>
