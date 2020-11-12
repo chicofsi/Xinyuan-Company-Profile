2a92c8a92400a9e43fa797c92c8a933dfdd80fa4
Chico Finando Sutan Ibrahim <finandochico@gmail.com>
2020-11-12 09:16:01 +0700

    parting

---
+++ app/Views/Template/header.php
@@ -0,0 +1,41 @@
+<!-- ***** Header Area Start ***** -->
+<header class="header-area header-sticky background-header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="container">
+                    <div class="row">
+                    <div class="col-6">
+                        <a href="" class="logo">
+                            <img class="logo-img" src="images/logo.png"></img>
+                        </a>
+                    </div>
+                    <div class="col-6">
+                        <div class="logo-right">
+                            <img class="logo-img" src="images/logo.png"></img>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="container-fluid nav-bg">
+                    <nav class="main-nav">
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li><a href="#welcome" class="menu-item">Home</a></li>
+                            <li><a href="#" class="menu-item">Company Profile</a></li>
+                            <li><a href="#" class="menu-item">Our Products</a></li>
+                            <li><a href="#" class="menu-item">Our Facilities</a></li>
+                            <li><a href="#" class="menu-item">Major Project</a></li>
+                            <li><a href="#" class="menu-item">Administration</a></li>
+                            <li><a href="#" class="menu-item">News</a></li>
+                            <li><a href="#" class="menu-item">Careers</a></li>
+                            <li><a href="#" class="menu-item">Contact Us</a></li>
+                        </ul>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
--- app/Views/index.php
+++ app/Views/index.php
@@ -2,78 +2,23 @@
 <html lang="en">
 
 <head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Haina Company Profile">
-    <meta name="author" content="Chico Finando">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    
+    <?= $this->include('template/meta') ?>
 
     <title>Diamond</title>
 
     
+    <?= $this->include('template/style') ?>
 
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-
-    <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
-
-    <link rel="stylesheet" href="css/owl-carousel.css">
-    <link rel="stylesheet" href="css/custom.css">
+    
 
 </head>
 <body>
 
-    <!-- ***** Preloader Start ***** -->
-    <div id="preloader">
-        <div class="jumper">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    </div>
-    <!-- ***** Preloader End ***** -->
-
-    <!-- ***** Header Area Start ***** -->
-    <header class="header-area header-sticky background-header">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="container">
-                    <div class="row">
-                    <div class="col-6">
-                        <a href="" class="logo">
-                            <img class="logo-img" src="images/logo.png"></img>
-                        </a>
-                    </div>
-                    <div class="col-6">
-                        <div class="logo-right">
-                            <img class="logo-img" src="images/logo.png"></img>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="container-fluid nav-bg">
-                    <nav class="main-nav">
-                        <!-- ***** Menu Start ***** -->
-                        <ul class="nav">
-                            <li><a href="#welcome" class="menu-item">Home</a></li>
-                            <li><a href="#" class="menu-item">Company Profile</a></li>
-                            <li><a href="#" class="menu-item">Our Products</a></li>
-                            <li><a href="#" class="menu-item">Our Facilities</a></li>
-                            <li><a href="#" class="menu-item">Major Project</a></li>
-                            <li><a href="#" class="menu-item">Administration</a></li>
-                            <li><a href="#" class="menu-item">News</a></li>
-                            <li><a href="#" class="menu-item">Careers</a></li>
-                            <li><a href="#" class="menu-item">Contact Us</a></li>
-                        </ul>
-                        <!-- ***** Menu End ***** -->
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </header>
-    <!-- ***** Header Area End ***** -->
+    
+    <?= $this->include('template/preloader') ?>
+    <?= $this->include('template/header') ?>
+    
     
 
     <div class="container">
@@ -132,25 +77,11 @@
         </div>
     </div>
 
+    
 
 
 
-    <!-- jQuery -->
-    <script src="js/jquery-2.1.0.min.js"></script>
-
-    <!-- Bootstrap -->
-    <script src="js/popper.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Plugins -->
-    <script src="js/owl-carousel.js"></script>
-    <script src="js/scrollreveal.min.js"></script>
-    <script src="js/waypoints.min.js"></script>
-    <script src="js/jquery.counterup.min.js"></script>
-    <script src="js/imgfix.min.js"></script>
-
-    <!-- Global Init -->
-    <script src="js/custom.js"></script>
+    <?= $this->include('template/script') ?>
 
     
 
